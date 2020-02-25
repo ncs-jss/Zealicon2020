50a0a49bfe96f3033ae0ef4a0c27016856a5bcb1
kunalvishnoi <vishnoi.kunal0@gmail.com>
2020-02-25 22:37:26 +0530

    Updates

---
+++ precache-manifest.3df5240218d974ee26efb294053075d1.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0028b08ea5adf2d72a06bef0c904ade",
+    "url": "/Zealicon2020/index.html"
+  },
+  {
+    "revision": "00544b2d6fdab6718819",
+    "url": "/Zealicon2020/static/css/2.85658d74.chunk.css"
+  },
+  {
+    "revision": "5045241069130e881576",
+    "url": "/Zealicon2020/static/css/main.703730cb.chunk.css"
+  },
+  {
+    "revision": "00544b2d6fdab6718819",
+    "url": "/Zealicon2020/static/js/2.63eb140b.chunk.js"
+  },
+  {
+    "revision": "486c864dbeea4f82b0912c66e3826be4",
+    "url": "/Zealicon2020/static/js/2.63eb140b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5045241069130e881576",
+    "url": "/Zealicon2020/static/js/main.9c5614e2.chunk.js"
+  },
+  {
+    "revision": "11084e3ad2ba51d59ad8",
+    "url": "/Zealicon2020/static/js/runtime-main.2e9465cd.js"
+  },
+  {
+    "revision": "77ddfe43c7892d4385491279f2d30b4b",
+    "url": "/Zealicon2020/static/media/1-1.77ddfe43.jpg"
+  },
+  {
+    "revision": "a065cb747e3194b678eded56dfa8cbc0",
+    "url": "/Zealicon2020/static/media/1.a065cb74.mp3"
+  },
+  {
+    "revision": "74cea90bfcb1041fde9be13635ef965a",
+    "url": "/Zealicon2020/static/media/2.74cea90b.mp3"
+  },
+  {
+    "revision": "f68a4aa98f9ae7bc77e8fd12fdcfe6b4",
+    "url": "/Zealicon2020/static/media/2.f68a4aa9.jpg"
+  },
+  {
+    "revision": "ad067660c99035d733e49068e69fe1fc",
+    "url": "/Zealicon2020/static/media/3-2.ad067660.jpg"
+  },
+  {
+    "revision": "0d8293b6a201c886224e8e71fd0922e7",
+    "url": "/Zealicon2020/static/media/3.0d8293b6.mp3"
+  },
+  {
+    "revision": "781537254faf4ab1c369373b385d6ce0",
+    "url": "/Zealicon2020/static/media/4-1.78153725.jpg"
+  },
+  {
+    "revision": "6e607dbff5bd4d29d824d0e6e19b3c73",
+    "url": "/Zealicon2020/static/media/4.6e607dbf.mp3"
+  },
+  {
+    "revision": "60246921b741897589d8f50c17a83c9e",
+    "url": "/Zealicon2020/static/media/5-1.60246921.jpg"
+  },
+  {
+    "revision": "73bda7fa2a62489a7d8c3ae1d4b9a376",
+    "url": "/Zealicon2020/static/media/5.73bda7fa.mp3"
+  },
+  {
+    "revision": "a7c2ab03928cf400b48e96bde6f6e79c",
+    "url": "/Zealicon2020/static/media/5.a7c2ab03.jpg"
+  },
+  {
+    "revision": "374dc97b929d5b1cf135deca61379264",
+    "url": "/Zealicon2020/static/media/brochure.374dc97b.pdf"
+  },
+  {
+    "revision": "efc05b0739c21a22811ec7b050a1b09f",
+    "url": "/Zealicon2020/static/media/coderz.efc05b07.jpg"
+  },
+  {
+    "revision": "00333e6336419f065a0329d753ecb290",
+    "url": "/Zealicon2020/static/media/coloralo.00333e63.jpg"
+  },
+  {
+    "revision": "b69d785f8ea890121ad51619b9244aa7",
+    "url": "/Zealicon2020/static/media/eve.b69d785f.jpg"
+  },
+  {
+    "revision": "f14eee643541cf03a10f26c944cc29f5",
+    "url": "/Zealicon2020/static/media/font.f14eee64.otf"
+  },
+  {
+    "revision": "31f4ecad805057960455418cec53cf85",
+    "url": "/Zealicon2020/static/media/main.31f4ecad.gif"
+  },
+  {
+    "revision": "da4a19cdaa3389570a65f36845753a37",
+    "url": "/Zealicon2020/static/media/mecha.da4a19cd.jpg"
+  },
+  {
+    "revision": "a328bfc7c42a0765235190c1548101ba",
+    "url": "/Zealicon2020/static/media/phone1.a328bfc7.jpg"
+  },
+  {
+    "revision": "6ef7d8681fbeaeea24eeb67c0e39dca6",
+    "url": "/Zealicon2020/static/media/phone3.6ef7d868.jpg"
+  },
+  {
+    "revision": "9b5ea77462f0d8cc363c9c46c99e7f16",
+    "url": "/Zealicon2020/static/media/phone4.9b5ea774.jpg"
+  },
+  {
+    "revision": "32dacadbf1158ea5c83208d3b06c12b3",
+    "url": "/Zealicon2020/static/media/phone5.32dacadb.jpg"
+  },
+  {
+    "revision": "72c67ff3e3f777194af46e09b8a179d2",
+    "url": "/Zealicon2020/static/media/play.72c67ff3.jpg"
+  },
+  {
+    "revision": "6695b2eec478dfcb35f499d0dcbe502b",
+    "url": "/Zealicon2020/static/media/robo.6695b2ee.jpg"
+  },
+  {
+    "revision": "ab8c0efaf728a332df1a0b4431078631",
+    "url": "/Zealicon2020/static/media/wars.ab8c0efa.jpg"
+  }
+]);
